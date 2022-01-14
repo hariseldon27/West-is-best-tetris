bb6293959628205215b2784616e083157897eea9
Max Sealey <maxsealey@users.noreply.github.com>
2022-01-14 00:26:04 -0700

    good starting point

---
+++ js-components/app.js
@@ -0,0 +1,105 @@
+/**********************************************
+ * Tetris Board version 1
+ * 
+ * ********************************************/
+
+function returnsTetrisBoard() {
+
+    const fullBoard = []
+
+    const row1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // Each cell in the array (indexes 0-9) should contain either 1 (truthy) or 0 (falsy)
+    const row2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // 1 if the cell is being occupied by part of the tetris piece, 0 if not
+    const row3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    const row4 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    const row5 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    const row6 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    const row7 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    const row8 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    const row9 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // this was a pain in the a** to write
+    const row10 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
+    const row11 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    const row12 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    const row13 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    const row14 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    const row15 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    const row16 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // what if each cell held an object, so that it could store more information? 
+                                                // What kind of information 
+
+    (function combineBoardsintoArray() {
+        return 
+    })() // an array method?
+
+    console.log(fullBoard)
+    console.table(fullBoard)
+
+    return fullBoard; //  
+}
+
+/*
+Square Block incoming
+[0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+[0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+Thank god finally
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+Ugh
+[0, 0, 0, 1, 1, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+*/
+
+console.log(returnsTetrisBoard()) // logs to the console the return value of the returnsTetrisBoard function
+                                  // the function is called while being passed into the log function method on the console object 
+
+
+function startGame() {
+
+    const play = 1 
+
+    while(play) {
+        // do stuff
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        !play
+    }
+}
